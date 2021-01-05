498cc862723b240f6837576eac7ebcbbfac32c8d
That Guy Cal <calmanning@gmail.com>
2021-01-04 21:11:09 -0800

    added way too much too quickly. Everything seems to be working so far. Got the button to the login, the login page, and the inverted page up. Next to add day-planner.

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ $(document).ready(function(){
     
     console.log("But why are you here?");
 
-    confirm("'Well, this is unexpected'");
+    //confirm("'Well, this is unexpected'");
 
     console.log("you have to leave.");
 
-    var userEntry = prompt("If you want to go any further\n...tell me what you want")
+    //var userEntry = prompt("If you want to go any further\n...tell me what you want")
         if (userEntry !== "duck") {
             while (userEntry !== "duck") {
                 userEntry = prompt('No, seriously, we need to know. \n \n It better not be about duck.');}
         }  
         console.log("NOW");
-        
-    screenEl.text("You shouldn't have done that.");
-        
 
-
+    $("#submit").on("click",function (event){
+        if ()
+    })
+    
 }) //document ready end. End of script
