c088d82643f7e379a78bd33fc973638c884bdd05
That Guy Cal <calmanning@gmail.com>
2021-01-05 21:50:56 -0800

    added day planner. Changed some of the header nav in the invertdex page.

--- script.js
+++ script.js
@@ -8,15 +8,67 @@ $(document).ready(function(){
 
     console.log("you have to leave.");
 
-    //var userEntry = prompt("If you want to go any further\n...tell me what you want")
-        if (userEntry !== "duck") {
-            while (userEntry !== "duck") {
-                userEntry = prompt('No, seriously, we need to know. \n \n It better not be about duck.');}
-        }  
-        console.log("NOW");
-
-    // $("#submit").on("click",function (event){
-    //     if ()
-    // })
+    $(document).ready(function () {
+        // an attempt at retreving localstorage, but it was proving to be unsuccessful. I think I'll have to go for the JSONstringify/parse method
+        $("#9am .plan").val(localStorage.getItem("9oclock"));
+        $("#10am .plan").val(localStorage.getItem("10oclock"));
+        $("#11am .plan").val(localStorage.getItem("11oclock"));
+        $("#12am .plan").val(localStorage.getItem("12oclock"));
+        $("#1pm .plan").val(localStorage.getItem("1oclock"));
+        $("#2pm .plan").val(localStorage.getItem("2oclock"));
+        $("#3pm .plan").val(localStorage.getItem("3oclock"));
+        $("#4pm .plan").val(localStorage.getItem("4oclock"));
+        $("#5pm .plan").val(localStorage.getItem("5oclock"));
+    
+        // Time that shows at the top of the page. It will be different that the operating time, which is very european at the moment.
+        var time = moment().format("dddd, hh:mm");
+        // console.log(`time: ${time}`);
+        $("#currentDay").append(time);
+    
+    // timeCheck() is a function that weighs the current pinged time (through moment's API) and compares that to the time slots on the scheduler -- it assigns their class (their color) based on whether or not the current time is "equal to", "less than", or "greater than" the pinged time.
+        function timeCheck() {
+            var dayHour = moment().format("k")
+            console.log(dayHour)
+            $(".plan").each(function () {
+                var blockTime = parseInt($(this).attr("data-time"));
+                
+    
+                if (dayHour === blockTime) {
+                    $(this).removeClass("past");
+                    $(this).removeClass("future");
+                    $(this).addClass("present");
+                } else if (dayHour < blockTime) {
+                    $(this).removeClass("past");
+                    $(this).removeClass("present");
+                    $(this).addClass("future");
+                } 
+                 else {
+                    $(this).removeClass("future");
+                    $(this).removeClass("present");
+                    $(this).addClass("past");
+                }
+                
+                if (dayHour <= 22) {
+                   $("#17").text("This is a MONDO hint.")
+                 }
+    
+                
+            })
+        }
+        timeCheck()
+    // This is the "save" to local data function. Which works! I just...can't seem to get the data correctly after it's saved.
+        $("button").click(function (event) {
+    
+            
+            var activity = $(this).siblings(".textarea").val();
+            var theHour = $(this).attr("data-todo");
+            
+    
+            localStorage.setItem(theHour, activity);
+    
+        })
+    
+    
+    }) //don't delete this. It's very important.
     
 }) //document ready end. End of script
